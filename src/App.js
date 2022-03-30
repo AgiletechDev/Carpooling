ff2cb57638f2f420a76757a51e5aa1979649517b
Agile-julio <102550217+Agile-julio@users.noreply.github.com>
2022-03-30 09:15:43 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import { AppRouter } from './routers/AppRouter';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+
 export const App = () => {
   return <AppRouter />;
 };
