7ac525d14560ee2276ddc1c8e0a2c28ddf7f29ff
Agile-julio <102550217+Agile-julio@users.noreply.github.com>
2022-03-30 10:40:05 -0500

    Update HomeScreen.js

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,5 +1,90 @@
 import React from 'react';
 
 export const HomeScreen = () => {
-  return <div>HomeScreen</div>;
+  return (
+    <div>
+      <div className="container-fluid">
+        <div className="row bg-primary justify-content-center text-light py-3">
+          <div className="col-auto col-sm-6 text-last">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house-door-fill" viewBox="0 0 16 16">
+              <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
+            </svg>
+            <a>Carpooling App</a>
+          </div>
+          <div className="col-auto">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+              <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+              <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+            </svg>
+            <a>USER</a>
+          </div>
+          <div className="col-auto">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bell-fill" viewBox="0 0 16 16">
+              <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>
+            </svg>
+          </div>
+        </div>
+      </div>
+
+      <div className="container-fluid">
+        <div className="row bg-info pt-5 pb-3 justify-content-center">
+          <div className="col-auto">
+            <div className="bg-light text-dark rounded-3 p-3 mb-3">
+              <div className="my-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                  <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+                </svg>
+                <label className="">Ciudad</label>
+              </div>
+              <input type="text" className="input-header border-0 border-bottom border-dark bg-transparent" placeholder="ciudad"/>
+            </div>
+          </div>
+
+          <div className="col-auto">
+            <div className="bg-light text-dark rounded-3 p-3 mb-3">
+              <div className="my-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                  <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+                </svg>
+                <label className="">Destino</label>
+              </div>
+              <input type="text" className="border-0 border-bottom border-dark bg-transparent" placeholder="destino"/>
+            </div>
+          </div>
+
+          <div className="col-auto">
+            <div className="bg-light text-dark rounded-3 p-3 mb-3">
+              <div className="my-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar2-week-fill" viewBox="0 0 16 16">
+                  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zm9.954 3H2.545c-.3 0-.545.224-.545.5v1c0 .276.244.5.545.5h10.91c.3 0 .545-.224.545-.5v-1c0-.276-.244-.5-.546-.5zM8.5 7a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm3 0a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM3 10.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z"/>
+                </svg>
+                <label className="">Fecha</label>
+              </div>
+              <input type="date" className="border-0 border-bottom border-dark bg-transparent"/>
+            </div>
+          </div>
+
+          <div className="col-auto align-self-center">
+            <button type="button" className="btn btn-primary px-4">Buscar <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+            </svg></button>
+          </div>
+        </div>
+      </div>
+
+      <div className="container-fluid">
+        <div className="row bg-light py-5 justify-content-center">
+          <div className="col-auto col-4 border border-success"></div>
+          <div className="col-auto">
+            <p className="h2 text-primary">¿Cansado de viajar solo?</p>
+            <ul className="list-unstyled text-primary text-center">
+              <li>Conoce nueva gente</li>
+              <li>Convierte tu vieja más</li>
+              <li>divertido y economico.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
